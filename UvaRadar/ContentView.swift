bc57181f21191e3f68f1cb7b3c1197803a911948
Federico Mete <fedee.1989@gmail.com>
2026-03-15 17:39:24 -0300

    Initial Commit

---
+++ UvaRadar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UvaRadar
+//
+//  Created by Federico Mete on 15/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UvaRadar/UvaRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  UvaRadarApp.swift
+//  UvaRadar
+//
+//  Created by Federico Mete on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct UvaRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
